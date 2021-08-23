a6391a9a770998240551d4147338aa9f643f67be
Preet Ojha <ozapreet08@gmail.com>
2021-08-23 22:01:03 +0530

    commit

--- main.js
+++ main.js
@@ -13,3 +13,34 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+$(window).scroll(function(){
+
+    $('header').toggleClass('scrolled',$(this).scrollTop()>50);
+
+});
+
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav ul li a');
+
+window.onscroll = () =>{
+
+    section.forEach(sec =>{
+        
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links =>{
+
+                links.classList.remove('active');
+                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
+
+            });
+        };
+
+    });
+
+}; 
