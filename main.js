57e4286cf0d8a7a92e52b212c810c97080e3e2a5
Preet Ojha <ozapreet08@gmail.com>
2021-08-24 00:54:16 +0530

    Half Done Commit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-let menu = document.querySelector('#menu_icon'); 
-let navbar = document.querySelector('.nav_bar');
-
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menu.onclick = () =>{
 
@@ -8,39 +7,7 @@ menu.onclick = () =>{
     navbar.classList.toggle('active');
 }
 
-  
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
-
-$(window).scroll(function(){
-
-    $('header').toggleClass('scrolled',$(this).scrollTop()>50);
-
-});
-
-let section = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav ul li a');
-
-window.onscroll = () =>{
-
-    section.forEach(sec =>{
-        
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navlinks.forEach(links =>{
-
-                links.classList.remove('active');
-                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
-
-            });
-        };
-
-    });
-
-}; 
