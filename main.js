fa0405772e29381f77d70054dcc58de008bc9256
Preet Ojha <ozapreet08@gmail.com>
2021-08-28 16:15:50 +0530

    Nav_Scroll Commit

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+const sections = document.querySelectorAll("section");
+// header nav a header ul li a
+const navLi = document.querySelectorAll("header ul li a");
+// const navLi = document.querySelectorAll("nav .container ul li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
